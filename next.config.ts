ffc1106cdc0f780dcb2dad29d371054100139f8d
connectwd <enquiries.connectedwd@outlook.com>
2025-01-16 15:39:41 +0000

    tidy up

--- next.config.ts
+++ next.config.ts
@@ -6,38 +6,6 @@ const nextConfig: NextConfig = {
     includePaths: [path.join(__dirname, 'styles')],
   },
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/rss.xml",
-        destination: "/feed/rss.xml",
-      },
-      {
-        source: "/atom.xml",
-        destination: "/feed/atom.xml",
-      },
-      {
-        source: "/feed.json",
-        destination: "/feed/feed.json",
-      },
-      {
-        source: "/rss",
-        destination: "/feed/rss.xml",
-      },
-      {
-        source: "/feed",
-        destination: "/feed/rss.xml",
-      },
-      {
-        source: "/atom",
-        destination: "/feed/atom.xml",
-      },
-      {
-        source: "/json",
-        destination: "/feed/feed.json",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
