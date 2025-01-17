af70f988f69bd6c499fee7368056f4903151192d
connectwd <enquiries.connectedwd@outlook.com>
2025-01-17 22:23:54 +0000

    add bearer token to api

---
+++ src/app/helpers/api/tokenCheck.ts
@@ -0,0 +1,17 @@
+/**
+ * Compares whether the submited token matches environmental authentication token
+ * @param { string } token
+ * @returns { boolean } True or False
+ */
+export function isCorrectToken(headers: Headers) {
+    const headerList = new Headers(headers);
+    const authHeader = headerList.get('authorisation');
+  
+    if (!authHeader || authHeader !== authToken) {
+      return false;
+    }
+  
+    return true;
+  }
+  
+  export const authToken: string = `Bearer ${process.env.BEARER_TOKEN}`;
